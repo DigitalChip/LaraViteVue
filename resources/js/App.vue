69c84d0a3b5c89f396588f3d4bb39a03adaad9de
DigitalChip <xsidmail@gmail.com>
2022-11-22 00:21:49 +0300

    worked version

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -15,6 +15,7 @@ import Navbar from './Components/Header/Navbar.vue'
 
 export default {
     name: 'app',
+
     components: {
         Navbar
         // 'Navbar': ()=> import ('./Components/Header/Navbar.vue')
--- resources/js/Components/Pages/Home.vue
+++ resources/js/Components/Pages/Home.vue
@@ -6,19 +6,45 @@
         <button class="btn btn-primary" @click="$store.commit('INCREMENT')">INCREMENT </button>
         <button class="btn btn-secondary" @click="$store.commit('DECREMENT')">Decrement </button>
     </div>
+    <p>Name is: {{ user.data.user.name }}</p>
 </template>
 
 
 <script>
-    import {mapState} from 'vuex'
-    export default {
-        name: 'home',
-        computed: {
-            ...mapState({
-                count: state => state.count
-            })
+import { mapState } from 'vuex'
+
+export default {
+    name: 'home',
+    computed: {
+        ...mapState({
+            count: state => state.count
+        })
+    },
+    data() {
+        return {
+            user: {
+                data: {user: {name:{}}}
+            }
         }
-    }
+    },
+    mounted() {
+        try {
+            this.getUser(5)
+        } catch (e) {
+            console.log('error');
+        }
+    },
+    methods: {
+        // url: 'http://laraprj.loc/api/v1/user/${id}'
+        getUser(id) {
+            axios({
+                url: "http://laraprj.loc/api/v1/user/" + id,
+                responseType: 'json'
+            }).then(response => (this.user = response));
+
+        },
+    },
+}
 </script>
 
 <style lang="">
