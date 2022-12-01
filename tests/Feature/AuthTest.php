dae1352b9167faaf01c5bf707e69e273fdc39e5f
DigitalChip <xsidmail@gmail.com>
2022-12-01 23:26:21 +0300

    [Back] Auth test add

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Str;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
@@ -10,6 +13,7 @@ class AuthTest extends TestCase
 
     public const API_BASE_URL = 'http://laraprj.loc/api/v1';
     public const API_URL_LOGIN = self::API_BASE_URL . '/login';
+    public const API_URL_REGISTER = self::API_BASE_URL . '/register';
 
     /**
      * A basic test example.
@@ -73,8 +77,74 @@ class AuthTest extends TestCase
             ->assertStatus(200)
             ->assertJson(['status' => true])
             ->assertJsonStructure(['data' => ['token']])
-            ->assertJsonStructure(['data' => ['user']]);;
+            ->assertJsonStructure(['data' => ['user']]);
     }
+
+    public function testRegisterWithoutData()
+    {
+        $response = $this->post(AuthTest::API_URL_REGISTER);
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure(
+                [
+                    'status',
+                    'message',
+                    'data',
+                ])
+            ->assertJson(['status' => false])
+            ->assertJsonStructure(['data' => ['email']])
+            ->assertJsonStructure(['data' => ['password']]);
+    }
+
+    public function testRegisterWithBadName()
+    {
+        $response = $this->post(AuthTest::API_URL_REGISTER, ['name'=>'','email' => 'email', 'password' => 'password', 'password_confirmation' => 'password'])
+            ->assertStatus(422)
+            ->assertJson(['status' => false])
+            ->assertJsonStructure(['data' => ['name']]);
+    }
+
+    public function testRegisterWithBadEmail()
+    {
+        $response = $this->post(AuthTest::API_URL_REGISTER, ['name'=>'name','email' => 'email', 'password' => 'password', 'password_confirmation' => 'password'])
+            ->assertStatus(422)
+            ->assertJson(['status' => false])
+            ->assertJsonStructure(['data' => ['email']]);
+    }
+
+    public function testRegisterWithBadPassword()
+    {
+        $response = $this->post(AuthTest::API_URL_REGISTER, ['name'=>'name','email' => 'email@email.com', 'password' => 'p', 'password_confirmation' => '1'])
+            ->assertStatus(422)
+            ->assertJson(['status' => false])
+            ->assertJsonStructure(['data' => ['password']]);
+    }
+
+    public function testRegisterWithBadPasswordConfirmation()
+    {
+        $response = $this->post(AuthTest::API_URL_REGISTER, ['name'=>'name','email' => 'email@email.com', 'password' => 'password', 'password_confirmation' => '1'])
+            ->assertStatus(422)
+            ->assertJson(['status' => false])
+            ->assertJsonStructure(['data' => ['password']]);
+    }
+
+    public function testRegisterWithRightCredentials()
+    {
+        $user = [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password_confirmation' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ];
+
+        $response = $this->post(AuthTest::API_URL_REGISTER, $user)
+            ->assertStatus(200)
+            ->assertJson(['status' => true])
+            ->assertJsonStructure(['data' => ['token']])
+            ->assertJsonStructure(['data' => ['user']]);
+    }
+
 
 
 }
